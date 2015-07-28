4c54900d930bc306da7264225b57ecf0ae44a980
MasoodGit <mohamed.alam78@gmail.com>
2015-07-28 22:57:36 +0530

    instagram api integration

--- app/app.js
+++ app/app.js
@@ -1,13 +1,42 @@
 angular.module('ngSearchGram', ['ngMessages'])
-.controller('ngSearchController',function($scope) {
+.controller('ngSearchController',function($scope,$http) {
+
+
+
+function InstaGramAPI(tag) {
+  var baseUrl = "https://api.instagram.com/v1/tags/{tag}/media/recent";
+  var clientId = "d2406ec1a54649a4a9778cf5f5be1e1d";
+  var endpoint = baseUrl.replace(/{tag}/g,tag);
+  var config = {
+    'params' : {
+        'client_id' : clientId,
+        'count' : 20,
+        'callback' : 'JSON_CALLBACK'
+    }
+  };
+
+  $http.jsonp(endpoint, config).success(function(result){
+    console.log(result);
+    if(result.meta.code == 200) {
+      $scope.images = result.data;
+      $scope.imageCount = result.data.length;
+    } else {
+      console.log(result.meta.error_type);
+    }
+  }).error(function(){
+
+  });
+}
+
 
 $scope.keyword = "";
-
 $scope.statusMessage ="";
 
 $scope.interacted = function(field) {
   return $scope.keywordForm.$submitted || field.$touched;
 };
+
+
 
 $scope.searchImages = function() {
   //make call to the instagram api
@@ -17,6 +46,7 @@ $scope.searchImages = function() {
     $scope.keywordForm.$setUntouched();
     $scope.keyword = "";
     $scope.keywordForm.$setPristine();
+    InstaGramAPI(searchKeyword);
   }
 };
 
